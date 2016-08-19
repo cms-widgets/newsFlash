8dca5e038bea8deb54f2945fa2e67d0abf024ed0
wuxiongliu <xiong328160186@qq.com>
2016-08-19 10:40:50 +0800

    删除js中mock的数据

--- src/main/resources/js/widgetInfo.js
+++ src/main/resources/js/widgetInfo.js
@@ -29,37 +29,8 @@ CMSWidgets.initWidget({
                 var serial = $("option:selected").val();
                 $(".news-link").remove();
 
-                //var data = [];
-                //for (var i = 0; i < 10; i++) {
-                //    var title = "name" + Math.random();
-                //    var publishDate = "2016-08-17";
-                //    var obj = {};
-                //    obj.title = title;
-                //    obj.publishDate = publishDate;
-                //    data.push(obj);
-                //}
-                //
-                //for (var i = 0; i < data.length; i++) {
-                //    var title = data[i].title;
-                //    var publishDate = data[i].publishDate;
-                //    var $tr = $($(".item").html()).find("tr");
-                //    $tr.find(".news-title").text(title);
-                //    $tr.find(".news-publishDate").text(publishDate);
-                //    $(".dataList").append($tr);
-                //    $(".displayContent").append($tr);
-                //}
 
                 getDataSource("findLinkContent", serial, function (data) {
-
-                    //var data = [];
-                    //for(var i=0;i<10;i++){
-                    //    var title = "name"+Math.random();
-                    //    var publishDate = "2016-08-17";
-                    //    var obj = {};
-                    //    obj.title = title;
-                    //    obj.publishDate = publishDate;
-                    //    data.push(obj);
-                    //}
 
                     for (var i = 0; i < data.length; i++) {
                         var title = data[i].title;
@@ -85,18 +56,6 @@ CMSWidgets.initWidget({
             }
             $(".dataSource").append(optionHtml);
             /*]]>*/
-
-            //var newsTypeList = [];
-            //for (var i = 0; i < 10; i++) {
-            //    var name = "name" + Math.random();
-            //    var obj = {};
-            //    obj.name = name;
-            //    obj.serial = Math.random();
-            //    newsTypeList.push(obj);
-            //}
-
-            //name,serial
-
 
             this.selectChange();
 
